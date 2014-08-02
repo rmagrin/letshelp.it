7ca39abeeff2bf668cf8c57f56b363d2cecc74a4
Rafael Magrin <rafael.magrin@gmail.com>
2014-08-02 22:04:13 +0000

    Removed slugs table since friendly_id 4 doesn't use it anymore.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100910000000) do
+ActiveRecord::Schema.define(:version => 20140802220045) do
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
