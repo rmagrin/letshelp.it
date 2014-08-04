8de7c6a1649595fd1949a232dc2c1f6ceccad1cc
Rafael Magrin <rafael.magrin@gmail.com>
2014-08-03 21:56:01 -0300

    Reorganized bootstrap.sh

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-apt-get update
+function update_apt_repositories {
+  apt-get update -y
+}
 
-apt-get install -y vim curl
+function install_basic_packages {
+  apt-get install -y vim curl git
+}
 
-apt-get uninstall -y ruby1.9 ruby1.9.1
-apt-get install -y ruby1.8 ruby1.8-dev rubygems libsqlite3-dev build-essential nodejs
+function install_requirements {
+  # git is need for deployment with capistrano
+  # rails requirements build-essential, nodejs (for execjs), libsqlite3-dev, postgresql-dev
+  apt-get install -y nodejs build-essential libsqlite3-dev postgresql-server-dev-all
+}
 
-gem install bundler
+function install_ruby {
+  echo "Installing ruby..."
+  apt-get remove -y ruby1.9 ruby1.9.1
+  apt-get install -y ruby1.8 ruby1.8-dev rubygems
+  gem install bundler
+  echo "Done installing ruby."
+}
 
-pushd /vagrant && bundle install && popd
+update_apt_repositories
+install_basic_packages
+install_requirements
+install_ruby
