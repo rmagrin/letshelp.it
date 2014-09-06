440acdd3d4c0d7a8329ad9338693559537c5974f
Rafael Magrin <rafael.magrin@gmail.com>
2014-09-06 03:57:52 +0000

    Fixed test to use xhr method to avoid issues when migrating to rails 4.1

--- test/functional/organizations_controller_test.rb
+++ test/functional/organizations_controller_test.rb
@@ -48,7 +48,7 @@ class OrganizationsControllerTest < ActionController::TestCase
 
   should "show tags when there is a validation error while creating a organization" do
     put :create, :organization => {}
-    assert_response :success 
+    assert_response :success
   end
 
 
@@ -57,17 +57,17 @@ class OrganizationsControllerTest < ActionController::TestCase
     should "reply the organizations related to a name in the search term" do
       get :search, :q => 'Red Cross'
       assert_search_successful [organizations(:redcross)], assigns(:organizations)
-    
+
       get :search, :q => 'santa Casa de misericordia'
       assert_search_successful [organizations(:santa_casa)], assigns(:organizations)
-    
+
       get :search, :q => 'misericórdia'
       assert_search_successful [organizations(:santa_casa)], assigns(:organizations)
     end
-  
+
     should "reply the organizations related to a city in the search term" do
       get :search, :q => 'tangamandapio'
-    
+
       assert_search_successful [organizations(:wwf)], assigns(:organizations)
     end
 
@@ -75,35 +75,35 @@ class OrganizationsControllerTest < ActionController::TestCase
       get :search, :q => 'food'
       assert_search_successful [], assigns(:organizations)
     end
-  
+
     should "reply search filtered by the selected tags" do
       get :search, :q => 'r', :tag_ids => [tags(:cloth).id.to_s]
       assert_search_successful [organizations(:wwf)], assigns(:organizations)
     end
-  
+
     should "return all organizations with the given tags" do
       get :search, :tag_ids => [tags(:food).id.to_s]
       assert_search_successful [organizations(:redcross), organizations(:wwf), organizations(:xpto)], assigns(:organizations)
-    
+
       get :search, :tag_ids => [tags(:cloth).id.to_s, tags(:money).id.to_s]
       assert_search_successful [organizations(:wwf), organizations(:xpto), organizations(:redcross)], assigns(:organizations)
     end
-  
+
     should "be able to find cities with and without accent" do
       get :search, :q => 'são paulo'
       assert_search_successful [organizations(:greenpeace), organizations(:santa_casa)], assigns(:organizations)
-    
+
       get :search, :q => 'sao paulo'
       assert_search_successful [organizations(:greenpeace), organizations(:santa_casa)], assigns(:organizations)
     end
-  
+
     should "not allow SQL injection" do
       get :search, :q => "' or '1=1"
       assert_search_successful [], assigns(:organizations)
-      
+
       get :search, :q => "%%"
       assert_search_successful Organization.all, assigns(:organizations)
-      
+
       get :search, :q => "%sao paulo%"
       assert_search_successful [organizations(:greenpeace), organizations(:santa_casa)], assigns(:organizations)
 
@@ -111,19 +111,19 @@ class OrganizationsControllerTest < ActionController::TestCase
         get :search, :q => "%'); DELETE FROM ""organizations""; SELECT * FROM ""organizations"" WHERE name LIKE '%"
       end
     end
-  
+
   end
 
   should "be able to edit organization when match password" do
-    get :ajax_edit, :format => 'js', :id => organizations(:wwf).id, :password => organizations(:wwf).password
+    xhr :get, :ajax_edit, :format => 'js', :id => organizations(:wwf).id, :password => organizations(:wwf).password
     assert_response :success
   end
-  
+
   should "not be able to edit organization when password dont match" do
-    get :ajax_edit, :format => 'js', :id => organizations(:wwf).id, :password => "wrong_pass"
+    xhr :get, :ajax_edit, :format => 'js', :id => organizations(:wwf).id, :password => "wrong_pass"
     assert :status => :forbidden
   end
-  
+
   private
 
   def assert_search_successful(expected_results, received_results)
